f13614513aa94c7db2003ccf3fcd7dd329285798
Nigel Delaney <ndelaney@broadinstitute.org>
2014-07-17 13:02:33 -0400

    Ignore timestamp if compiled

--- src/MitoDataAssembler/ConsoleInterface/Program.cs
+++ src/MitoDataAssembler/ConsoleInterface/Program.cs
@@ -30,8 +30,11 @@ namespace MitoDataAssembler
 		if (Bio.CrossPlatform.Environment.RunningInMono) {
 				Console.WriteLine ("Running in the Mono runtime");
 		}
-            DateTime compTime = RetrieveLinkerTimestamp();
+        bool worked;
+        DateTime compTime = RetrieveLinkerTimestamp(out worked);
+        if(worked) {
 			Console.WriteLine("Compiled on: " +compTime.ToLongDateString()+" "+compTime.ToLongTimeString());
+        }
 #if !DEBUG
             try
             {
@@ -181,34 +184,41 @@ namespace MitoDataAssembler
             
         }
 
-        private static DateTime RetrieveLinkerTimestamp()
+        private static DateTime RetrieveLinkerTimestamp(out bool worked)
             {
+               
                 string filePath = System.Reflection.Assembly.GetCallingAssembly().Location;
-                const int c_PeHeaderOffset = 60;
-                const int c_LinkerTimestampOffset = 8;
-                byte[] b = new byte[2048];
-                System.IO.Stream s = null;
+                worked = false;
+                if (System.IO.File.Exists(filePath))
+                {
+                    worked = true;
+                    const int c_PeHeaderOffset = 60;
+                    const int c_LinkerTimestampOffset = 8;
+                    byte[] b = new byte[2048];
+                    System.IO.Stream s = null;
 
-                try
-                {
-                    s = new System.IO.FileStream(filePath, System.IO.FileMode.Open, System.IO.FileAccess.Read);
-                    s.Read(b, 0, 2048);
-                }
-                finally
-                {
-                    if (s != null)
+                    try
                     {
-                        s.Close();
+                        s = new System.IO.FileStream(filePath, System.IO.FileMode.Open, System.IO.FileAccess.Read);
+                        s.Read(b, 0, 2048);
                     }
+                    finally
+                    {
+                        if (s != null)
+                        {
+                            s.Close();
+                        }
+                    }
+
+                    int i = System.BitConverter.ToInt32(b, c_PeHeaderOffset);
+                    int secondsSince1970 = System.BitConverter.ToInt32(b, i + c_LinkerTimestampOffset);
+                    DateTime dt = new DateTime(1970, 1, 1, 0, 0, 0);
+
+                    dt = dt.AddSeconds(secondsSince1970);
+                    dt = dt.AddHours(TimeZone.CurrentTimeZone.GetUtcOffset(dt).Hours);
+                    return dt;
                 }
-
-                int i = System.BitConverter.ToInt32(b, c_PeHeaderOffset);
-                int secondsSince1970 = System.BitConverter.ToInt32(b, i + c_LinkerTimestampOffset);
-                DateTime dt = new DateTime(1970, 1, 1, 0, 0, 0);
-
-                dt = dt.AddSeconds(secondsSince1970);
-                dt = dt.AddHours(TimeZone.CurrentTimeZone.GetUtcOffset(dt).Hours);
-                return dt;
+                return DateTime.Now;
             }
 		private static void PrintHelp(CommandLineArguments parser)
 		{
